3be7e0183ad30f698af5098e9754fb68aa4e00a3
FIONNOROURKE-STUDENT <G00429672@atu.ie>
2025-11-21 10:33:08 +0000

    test

--- src/test/java/org/example/cicd1_exam_friday1/test.java
+++ src/test/java/org/example/cicd1_exam_friday1/test.java
@@ -1,4 +1,19 @@
 package org.example.cicd1_exam_friday1;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class test {
+    private ticketService ticketService = new ticketService();
+    @BeforeEach
+    public void setup() {
+        org.example.cicd1_exam_friday1.ticketService ticketService = new org.example.cicd1_exam_friday1.ticketService();
+
+    }
+    @Test
+    void create thenfindById(){
+        ticketService ticketService = new ticketService();
+
+    }
+
 }
